7f4f78fb10bf9fce131e5a5e351d6ce727b98559
xonin-hush <xonindev@gmail.com>
2024-03-01 10:43:11 +0000

    Add priorityColor to change projects border colors

--- src/projects.js
+++ src/projects.js
@@ -1,46 +1,43 @@
 //this module should be responsible for all is it about Projects
 const dialog = document.querySelector("dialog")
-const addProject=document.querySelector("#add-project")
+const addProject = document.querySelector("#add-project")
 const submit = dialog.querySelector("#submit")
 
 class ProjectInfo {
   constructor(title, description, dueTime, priority) {
-      this.title = title
-      this.description = description
-      this.dueTime = dueTime
-      this.priority = priority
-  }
-  consoleLog() {
-      console.log(this.title, this.description, this.dueTime, this.priority)
+    this.title = title
+    this.description = description
+    this.dueTime = dueTime
+    this.priority = priority
   }
 }
 
 //takes info from "projects" and loops though it
 //handles projects on the DOM
-
 export function projectsWindow() {
-    const index = document.querySelector(".index")
-    index.innerHTML=""
-    const myProjects = document.createElement("h1")
-    myProjects.textContent = "My Projects"
-    index.appendChild(myProjects)
-    const projectsDiv = document.createElement("div")
-    let counter = 0
-    for (let projectIndex of projects) {
-        for (let projectProperty of projectIndex) {
-            if (typeof projectProperty != "number") {
-                console.log(projectProperty)
-                const project = document.createElement("div")
-                project.classList.add("card")
-                projectsDiv.classList.add("cards")
-                project.innerHTML = `<input type="checkbox" id="project" name="project" value="project${counter}">`
-                projectCard(projectProperty, project)
-                projectsDiv.appendChild(project)
-                index.appendChild(projectsDiv)
-            }
-            counter++
-        }
+  const index = document.querySelector(".index")
+  index.innerHTML = ""
+  const myProjects = document.createElement("h1")
+  myProjects.textContent = "My Projects"
+  index.appendChild(myProjects)
+  const projectsDiv = document.createElement("div")
+  let counter = 0
+  for (let projectIndex of projects) {
+    for (let projectProperty of projectIndex) {
+      if (typeof projectProperty != "number") {
+        console.log(projectProperty)
+        const project = document.createElement("div")
+        project.classList.add("card")
+        projectsDiv.classList.add("cards")
+        // project.innerHTML = `<input type="checkbox" id="project" name="project" value="project${counter}">`
+        priorityColor(project,projectProperty.priority)
+        projectCard(projectProperty, project)
+        projectsDiv.appendChild(project)
+        index.appendChild(projectsDiv)
+      }
+      counter++
     }
+  }
 }
 function projectCard(projectProperty, project) { //this function creates a card
   const projectTitle = document.createElement("div")
@@ -59,29 +56,44 @@ function projectCard(projectProperty, project) { //this function creates a card
 projectsWindow()
 
 function getProjectInfo() {
-    submit.addEventListener("click", (event) => {
-      event.preventDefault(); // We don't want to submit this fake form
-      const project=[]
-      var projectTitle = document.querySelector("#title-input")
-      var temp = projectTitle.value
-      var  projectDescription= document.querySelector("#description-input")
-      var projectDeadLine = document.querySelector("#date-input")
-      var projectPriority = document.querySelector("#priority-input")
-      console.log(projectTitle.value,projectDescription.value,projectDeadLine.value,projectPriority.value)
-      if (projectTitle.value && projectDescription.value && projectDeadLine.value &&projectPriority.value !== "") {
-        temp = new ProjectInfo(projectTitle.value , projectDescription.value ,projectDeadLine.value ,projectPriority.value, "on")
-        project.push(temp)
-        projects.push(project)
-        console.log(projects)
-        projectsWindow()
-        dialog.close(dialog.value); // Have to send the select box value here.
-      }
-    });
-  }
-  addProject.addEventListener("click", () => {
-    dialog.showModal()
+  submit.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    const project = []
+    var projectTitle = document.querySelector("#title-input")
+    var temp = projectTitle.value
+    var projectDescription = document.querySelector("#description-input")
+    var projectDeadLine = document.querySelector("#date-input")
+    var projectPriority = document.querySelector("#priority-input")
+    console.log(projectTitle.value, projectDescription.value, projectDeadLine.value, projectPriority.value)
+    if (projectTitle.value && projectDescription.value && projectDeadLine.value && projectPriority.value !== "") {
+      temp = new ProjectInfo(projectTitle.value, projectDescription.value, projectDeadLine.value, projectPriority.value, "on")
+      project.push(temp)
+      projects.push(project)
+      console.log(projects)
+      projectsWindow()
+      dialog.close(dialog.value); // Have to send the select box value here.
+    }
   });
+}
+addProject.addEventListener("click", () => {
+  dialog.showModal()
+});
 getProjectInfo()
+function priorityColor(project, projectPriority) {
+  if (projectPriority == "priority1") {
+    project.style.borderLeft = "10px solid #ff1717"
+  }
+  if (projectPriority == "priority2") {
+    project.style.borderLeft = "10px solid #ffe817"
+  }
+  if (projectPriority == "priority3") {
+    project.style.borderLeft = "10px solid #aeff17"
+  }
+  if (projectPriority == "priority4") {
+    project.style.borderLeft = "10px solid #17bdff"
+  }
+
+}
 // function doStuff() {
 // console.log(projects);
 //  }
