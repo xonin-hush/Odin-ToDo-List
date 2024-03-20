ebe34bd94cacdfdb445e815d97691b621f53511f
Xonin-hush <xonindev@gmail.com>
2024-03-20 18:14:33 -0400

    Make add todo button functional

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,15 +1,16 @@
 //this module should be responsible for all is it about ToDos
 window.projects = []//done
 //creates project that we're going to put todo s in it
-// class ToDo {
-//     constructor(title, description, dueTime, priority) {
-//         this.title = title
-//         this.description = description
-//         this.dueTime = dueTime
-//         this.priority = priority
-//     }
-// }
-
+class ToDo {
+    constructor(title, description, dueTime, priority) {
+        this.title = title
+        this.description = description
+        this.dueTime = dueTime
+        this.priority = priority
+    }
+}
+const todoDialog = document.querySelector(".todo-dialog")
+const submit=todoDialog.querySelector("#submit")
 export function showTodo() {
   window.addEventListener('click', (e) => {
     let toDo = e.target.getAttribute("class")
@@ -20,7 +21,7 @@ export function showTodo() {
       index.innerHTML = ""
       const todoHeader = document.createElement("div")
       const myTodos = document.createElement("h1")
-const addButton=addTodoButton()
+      const addButton = addTodoButton()
       myTodos.textContent = "My Todos"
       todoHeader.appendChild(myTodos)
       todoHeader.appendChild(addButton)
@@ -29,16 +30,40 @@ const addButton=addTodoButton()
 
 
     }
+    if (e.target.getAttribute("class") == "button add-todo-button") {
+      todoDialog.showModal()
+    }
   });
 }
 showTodo()
 function addTodoButton() {
   const addTodo = document.createElement("button")
   addTodo.classList.add("button")
+  addTodo.classList.add("add-todo-button")
   addTodo.textContent = "Add Todo"
   return addTodo
-  
+
 }
+function getTodoInfo() { //takes info from dialog form and sends it to projectsWindow
+  submit.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    var todoTitle = document.querySelector("#title-input")
+    var temp = todoTitle.value
+    var todoDescription = document.querySelector("#description-input")
+    var todoDeadLine = document.querySelector("#date-input")
+    var todoPriority = document.querySelector("#priority-input")
+    if (todoTitle.value && todoDescription.value && todoDeadLine.value && todoPriority.value !== "") {
+      temp = new ProjectInfo(projectTitle.value, projectDescription.value, projectDeadLine.value, projectPriority.value)
+      // temp = new ToDo("projectTitle.value", "projectDescription.value", "projectDeadLine.value", "projectPriority.value")
+      // this is setting the counter in the project array-->[projectinfo,counter]
+      console.log(projects)
+      showTodo()
+      todoDialog.close(todoDialog.value); // Have to send the select box value here.
+    }
+  });
+}
+getTodoInfo()
+
 function todoCard(project) {
   const todoTitle = document.createElement("div")
   todoTitle.textContent = projectProperty.title
@@ -54,6 +79,7 @@ function todoCard(project) {
   project.appendChild(todoPriority)
 
 }
+
 //checks if the element clicked is it's a card then do something about it
 // window.addEventListener('click', (e) => {
 // let element = e.target.getAttribute("class")
